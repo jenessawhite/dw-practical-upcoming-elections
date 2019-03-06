40b9f6703148a3c6a454bc6d59d7b0664f539e7f
Jenessa White <jenessaw31@gmail.com>
2019-03-06 13:35:26 -0600

    create methods for apiUrl and to fetch info

--- javascript/routes/index.js
+++ javascript/routes/index.js
@@ -1,6 +1,24 @@
 var express = require('express');
-var router = express.Router();
+var fetch = require('node-fetch');
 var us_states = require('../us_state.js');
+
+var router = express.Router();
+
+function apiUrl(state, place) {
+  return `https://api.turbovote.org/elections/upcoming?district-divisions=ocd-division/country:us/state:${state},ocd-division/country:us/state:${state}/place:${place}`;
+}
+
+function getElectionInfo(state, place) {
+  return fetch(apiUrl(state, place), {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json'
+    }
+  })
+    .then(res => res.json())
+    .then(electionInfo => electionInfo[0])
+    .catch(err => console.log(err));
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
